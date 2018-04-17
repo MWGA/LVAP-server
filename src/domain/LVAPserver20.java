98fabef2b532af7c12f0d7b5c526c5581e0556d3
denverve <katka4president@gmail.com>
2018-04-17 11:55:50 +0200

    LVAP server updated

--- src/domain/LVAPserver20.java
+++ src/domain/LVAPserver20.java
@@ -1,13 +1,14 @@
 /*
+ * Author Katarina Bedejova
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-   http://www.java2s.com/Code/Java/Network-Protocol/ASimpleWebServer.htm
-   https://www.codeproject.com/Tips/1040097/Create-simple-http-server-in-Java
-   https://github.com/iamprem/HTTPclient-server/blob/master/HTTPServer/src/Server.java
-   http://www.java2s.com/Code/Java/Network-Protocol/AverysimpleWebserverWhenitreceivesaHTTPrequestitsendstherequestbackasthereply.htm  --- robit to podla tohto
  */
 package domain;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,23 +24,25 @@ import java.sql.Statement;
 import static java.sql.Types.NULL;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class LVAPserver20 {
     
     //LVAP structure
     //declared as nested static class :
-    // When you declare a nested class static, it automatically becomes a stand alone class which can be instantiated without having to instantiate the outer class it belongs to.
-
-    static class LVAP_struct {
+    //When you declare a nested class static, it automatically becomes a stand alone class which can be instantiated without having to instantiate the outer class it belongs to.
+    public static class LVAP_struct {
 
         String IP_address;
-        byte[] MAC_address;
-        byte[]  BSSID;
+        String MAC_address;
+        String  BSSID;
         String SSID;
         int WTP;
 
-        public LVAP_struct(String ipaddr, byte[] macaddr, byte[] bssidaddr, String netssid, int wtpnum) {
+        public LVAP_struct(String ipaddr, String macaddr, String bssidaddr, String netssid, int wtpnum) {
             IP_address = ipaddr;
             MAC_address = macaddr;
             BSSID = bssidaddr;
@@ -52,12 +55,12 @@ public class LVAPserver20 {
     private static String tableName = "STRUCTURES";
     // jdbc Connection
     private static Connection con = null;
-    private static Statement stmt = null;
-    
+    private static Statement stmt = null;    
     static String uName = " ";
     static String uPass= " ";
     static ResultSet rs;
     
+    //calculate BSSID
     private static String getBSSID(String adresa) {
         String result = null;
         
@@ -66,7 +69,8 @@ public class LVAPserver20 {
         
         return result;
     }
-
+    
+    //establish a connection with the database
     private static void createConnection()
     {
         try
@@ -80,7 +84,8 @@ public class LVAPserver20 {
             except.printStackTrace();
         }
     }
-     
+    
+    //shutdown connection with the database
     private static void shutdown()
     {
         try
@@ -102,24 +107,282 @@ public class LVAPserver20 {
 
     }
     
-    public static boolean checkIP(String ip) throws SQLException {
+    //returns empty (null iniatilized) LVAP structure
+    static LVAP_struct createLVAP() {    
+        return (new LVAP_struct(null,null,null,null,0));
+    }
+    
+    //delete LVAP struct when no longer necessary
+    static void disposeOfLVAP(LVAP_struct deleteMe) {        
+        deleteMe = null;
+    }
+    
+    //process client request (LVAP server)
+    public static LVAP_struct processRequest(String req) throws SQLException {
+        System.out.println("Processing client request ...... ");
+        List<String> list = new ArrayList<String>(Arrays.asList(req.split(",")));
+              
+        LVAP_struct tmp = new LVAP_struct(list.get(0),list.get(1),list.get(2),list.get(3),0);
         
-    byte[] macadrr = ip.getBytes();
-
+        //update or insert?
+        //This solution expects that MAC address is ALWAYS specified in a request
+        if(checkForMac(tmp.MAC_address)) {
+            System.out.println("MAC known. Updating ......");
+            return updateLVAP(tmp);
+        }
+        else {
+            System.out.println("MAC unknown. Creating new LVAP structure......");
+            return insertLVAPstruct(tmp);
+        }
+            
+     }
+     
+    //update LVAP record in the database
+    public static LVAP_struct updateLVAP(LVAP_struct structure) throws SQLException {
+       
+        System.out.println("Update started ......");
+       //This solution expects that MAC address is ALWAYS specified in a request
+  
+       if(!structure.IP_address.equals("-")) {
+           System.out.println("Adding IP address ......");
+           System.out.println("Trying to add IP address ...." + structure.IP_address);
+           System.out.println("Corresponding MAC ..... " + structure.MAC_address);
+           String query = "UPDATE STRUCTURES SET IP=? WHERE MAC=?";
+           PreparedStatement ps = con.prepareStatement(query);
+           ps.setString(1, structure.IP_address);
+           ps.setString(2, structure.MAC_address);
+           ps.executeUpdate();          
+       }
+       
+       if(!structure.SSID.equals("-")) {
+           System.out.println("Adding SSID......");
+           String query = "UPDATE STRUCTURES SET SSID=? WHERE MAC=?";
+           PreparedStatement ps = con.prepareStatement(query);
+           ps.setString(1, structure.SSID);
+           ps.setString(2, structure.MAC_address);
+           ps.executeUpdate();
+           
+       }
+              
+       System.out.println("Updated ......");
+       return structure;
+    }
     
-    String sql = "Select 1 from STRUCTURES where MAC = ?";  
+    //method name self-explanatory
+    public static LVAP_struct retrieveStructByIP(String value) throws SQLException{
+        
+      LVAP_struct structure = createLVAP();
 
-    PreparedStatement ps = con.prepareStatement(sql);
-    //ps.setString(1, macaddr);
-    ps.setBytes(1, macadrr);
-    ResultSet rs = ps.executeQuery();
+      String query = "SELECT * FROM STRUCTURES WHERE IP = ?";
 
+      // create the java statement
+      PreparedStatement ps = con.prepareStatement(query);
+      ps.setString(1, value);
+      
+      // execute the query, and get a java resultset
+      ResultSet rs = ps.executeQuery(query);
+      
+      // iterate through the java resultset
+      while (rs.next())
+      {
+        String IP = rs.getString("IP");
+        String MAC = rs.getString("MAC");
+        String BSSID = rs.getString("BSSID");
+        String SSID = rs.getString("SSID");
+        int WTP = rs.getInt("WTP");
+        
+        //update the structure
+        structure.IP_address = IP;
+        structure.MAC_address = MAC;
+        structure.BSSID = BSSID;
+        structure.SSID = SSID;
+        structure.WTP = WTP;
+
+      }
+      return structure;
+      
+    }
     
-    return rs.next();
-}
+    //method name self-explanatory
+    public static LVAP_struct retrieveStructByMAC(String value) throws SQLException{
+        
+      LVAP_struct structure = createLVAP();
+        
+      String query = "SELECT * FROM STRUCTURES WHERE MAC = ?";
 
-    private static void insertLVAPstruct(LVAP_struct structure)
+      // create the java statement
+      PreparedStatement ps = con.prepareStatement(query);
+      ps.setString(1, value);
+      
+      // execute the query, and get a java resultset
+      ResultSet rs = ps.executeQuery(query);
+      
+      // iterate through the java resultset
+      while (rs.next())
+      {
+        String IP = rs.getString("IP");
+        String MAC = rs.getString("MAC");
+        String BSSID = rs.getString("BSSID");
+        String SSID = rs.getString("SSID");
+        int WTP = rs.getInt("WTP");
+        
+        //update the structure
+        structure.IP_address = IP;
+        structure.MAC_address = MAC;
+        structure.BSSID = BSSID;
+        structure.SSID = SSID;
+        structure.WTP = WTP;
+      }
+        
+        return structure;
+    
+     }
+    
+    //method name self-explanatory 
+    public static LVAP_struct retrieveStructByBSSID(String value) throws SQLException{
+        
+        LVAP_struct structure = createLVAP();
+        
+        String query = "SELECT * FROM STRUCTURES WHERE BSSID = ?";
+
+      // create the java statement
+      PreparedStatement ps = con.prepareStatement(query);
+      ps.setString(1, value);
+      
+      // execute the query, and get a java resultset
+      ResultSet rs = ps.executeQuery(query);
+      
+      // iterate through the java resultset
+      while (rs.next())
+      {
+        String IP = rs.getString("IP");
+        String MAC = rs.getString("MAC");
+        String BSSID = rs.getString("BSSID");
+        String SSID = rs.getString("SSID");
+        int WTP = rs.getInt("WTP");
+        
+        //update the structure
+        structure.IP_address = IP;
+        structure.MAC_address = MAC;
+        structure.BSSID = BSSID;
+        structure.SSID = SSID;
+        structure.WTP = WTP;
+        
+        }
+      
+      return structure;
+    }
+    
+    //method name self-explanatory
+    public static LVAP_struct retrieveStructBySSID(String value) throws SQLException{
+        
+        LVAP_struct structure = createLVAP();
+        
+        String query = "SELECT * FROM STRUCTURES WHERE SSID = ?";
+
+      // create the java statement
+      PreparedStatement ps = con.prepareStatement(query);
+      ps.setString(1, value);
+      
+      // execute the query, and get a java resultset
+      ResultSet rs = ps.executeQuery(query);
+      
+      // iterate through the java resultset
+      while (rs.next())
+      {
+        String IP = rs.getString("IP");
+        String MAC = rs.getString("MAC");
+        String BSSID = rs.getString("BSSID");
+        String SSID = rs.getString("SSID");
+        int WTP = rs.getInt("WTP");
+        
+        //update the structure
+        structure.IP_address = IP;
+        structure.MAC_address = MAC;
+        structure.BSSID = BSSID;
+        structure.SSID = SSID;
+        structure.WTP = WTP;
+      }
+        
+        return structure;
+    }
+    
+    //method name self-explanatory
+    public static LVAP_struct retrieveStructByWTP(int value) throws SQLException{
+        
+        LVAP_struct structure = createLVAP();
+        
+        String query = "SELECT * FROM STRUCTURES WHERE WTP = ?";
+
+      // create the java statement
+      PreparedStatement ps = con.prepareStatement(query);
+      ps.setInt(1, value);
+      
+      // execute the query, and get a java resultset
+      ResultSet rs = ps.executeQuery(query);
+      
+      // iterate through the java resultset
+      while (rs.next())
+      {
+        String IP = rs.getString("IP");
+        String MAC = rs.getString("MAC");
+        String BSSID = rs.getString("BSSID");
+        String SSID = rs.getString("SSID");
+        int WTP = rs.getInt("WTP");
+        
+        //update the structure
+        structure.IP_address = IP;
+        structure.MAC_address = MAC;
+        structure.BSSID = BSSID;
+        structure.SSID = SSID;
+        structure.WTP = WTP;
+      }
+        
+        return structure;
+    }
+    
+    //checks if a record with given MAC address exists in database
+    public static boolean checkForMac(String value) throws SQLException { 
+        
+        
+        System.out.println("Checking for mac ...... : " + value);
+        String sql = "Select 1 from STRUCTURES where MAC = ?"; 
+        System.out.println(" sql nastavene");
+        PreparedStatement prst = con.prepareStatement(sql);
+        System.out.println("prepared statement pripraveny");
+        prst.setString(1, value);
+        ResultSet rs = prst.executeQuery();
+        System.out.println("query executed");
+                
+        return rs.next();      
+    }
+    
+    //checks if a record with given IP address exists in database
+    public static boolean checkForIP(String value) throws SQLException { 
+        
+        String sql = "Select 1 from STRUCTURES where IP = ?";  
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, value);
+        ResultSet rs = ps.executeQuery();
+        
+        return rs.next();
+    }
+    
+    //checks if a record with given BSSID exists in database
+    public static boolean checkForBSSID(String value) throws SQLException { 
+        
+        String sql = "Select 1 from STRUCTURES where BSSID = ?";  
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, value);
+        ResultSet rs = ps.executeQuery();
+        
+        return rs.next();
+    }
+    
+    //inserts LVAP structure into database
+    private static LVAP_struct insertLVAPstruct(LVAP_struct structure)
     {
+        System.out.println("Insert started ......");
         try
         {
             stmt = con.createStatement();
@@ -136,10 +399,14 @@ public class LVAPserver20 {
         {
             sqlExcept.printStackTrace();
         }
+        
+        System.out.println("Inserted ......");
+        
+        return structure;
     }
     
-    
-        private static void selectLVAPstruct()
+    //debug function - list all DB entries into console
+    private static void selectLVAPstruct()
     {
         try
         {
@@ -172,121 +439,63 @@ public class LVAPserver20 {
             sqlExcept.printStackTrace();
         }
     }
-        
-        
-        
-    
-    protected static boolean process(String MAC_ADRESA) throws SQLException {
-          
-        boolean check = checkIP(MAC_ADRESA);
-        return check;
-    }
-    
-
+       
+    //server thread
     protected static void run() throws IOException, SQLException {
-      
-    
-    
+         
     ServerSocket s = new ServerSocket(80);
+    LVAP_struct responseObj; //reference to the LVAP struct that will be later sent to a client as JSON object
     
-    System.out.println("Cakam vas na porte 8080");
-    System.out.println("Pripojte sa niekto ");
-    System.out.println("Poslite mi JSON ");
-
-    
+    System.out.println("Cakam vas na porte 80 :) ");
+    System.out.println("Pripojte sa niekto");
+    System.out.println("Server waiting for request ..... ");
     
     while(true) {
         
         // wait for a connection
         Socket client = s.accept(); 
-        System.out.println("niekto sa pripojil");
-        
-        
+        System.out.println("Client connection accepted ......");
+               
         // Get input and output streams to talk to the client
         BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
         PrintWriter out = new PrintWriter(client.getOutputStream());
         
-        //Get client request - MAC address
-        String client_request = ".";
-        while (!client_request.equals(""))
-            client_request = in.readLine();
+        String request = in.readLine();
+
+        responseObj = processRequest(request);
         
-        // Start sending our reply, using the HTTP 1.1 protocol
-        out.print("HTTP/1.1 200 \r\n"); // Version & status code
-        out.print("Content-Type: text/plain\r\n"); // The type of data
-        out.print("Connection: close\r\n"); // Will close stream
-        out.print("\r\n"); // End of headers
-                
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd' 'HH:mm:ss").create();
+        // JSON data structure
+        JsonElement jsonElement = gson.toJsonTree(responseObj);
+        JsonObject jsonObject = (JsonObject) jsonElement;
+        // property removal
+        //jsonObject.remove("property");
+        // serialization to String
+        String javaObjectString = jsonObject.toString();
+        System.out.println(javaObjectString);
+        //send to the client
+        //out.print(javaObjectString);
         
-        if (process(client_request)){
-            out.print("N");
-        }
-        else {
-            out.print("Y");
-            //LVAP_struct tmp = new LVAP_struct();
-            //insertLVAPstruct(tmp);
-            //zatial nemam dalsie parametre k dispozicii......
-         }
-        
-        
-        out.flush(); //splachnut po sebe
-            
+        out.flush(); //splachnut po sebe            
         out.close(); 
-        in.close();
-        
+        in.close();        
         client.close();
         
-    }
-    
-    
-    }
+    }        
+}
         
     /**
+     * MAIN FUNCTION
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, SQLException {
         
+        createConnection();
+        
         LVAPserver20 server = new LVAPserver20();
         server.run();
         
-                //first test entry
-        String ssid1 = "eduroam";
-        String ip1 = "187.150.55.2";
-        String mac01 = "9f:0a:35:ef:68:16";
-        String bssid01 = "ee:ee:cc:cd:18:e5";
-        byte[] mac1 = mac01.getBytes();
-        byte[] bssid1 = bssid01.getBytes();
-        LVAP_struct sulec = new LVAP_struct(ip1,mac1,bssid1,ssid1,0);
-
-        //second test entry
-        String ssid2 = "Guest";
-        String ip2 = "152.158.55.5";
-        String mac02 = "55:5a:a5:5a:aa:8a";
-        String bssid02 = "ee:ee:aa:91:aa:aa";
-        byte[] mac2 = mac02.getBytes();
-        byte[] bssid2 = bssid02.getBytes();
-        LVAP_struct hepi = new LVAP_struct(ip2,mac2,bssid2,ssid2,1);
-
-
-        //third test entry
-        String ssid3 = "vraH";
-        String ip3 = "210.210.215.28";
-        String mac03 = "45:08:8b:8d:d8:e6";
-        String bssid03 = "ee:ee:ff:ff:ee:ee";
-        byte[] mac3 = mac03.getBytes();
-        byte[] bssid3 = bssid03.getBytes();
-        LVAP_struct opicka = new LVAP_struct(ip3,mac3,bssid3,ssid3,1);
-
-        //fourth test entry
-        String ssid4 = "Sulec 5G";
-        String ip4 = "12.20.6.155";
-        String mac04 = "ab:ca:55:f3:fb:c7";
-        String bssid04 = "ee:ee:c4:da:da:57";
-        byte[] mac4 = mac04.getBytes();
-        byte[] bssid4 = bssid04.getBytes();
-        LVAP_struct trubicka = new LVAP_struct(ip4,mac4,bssid4,ssid4,1);
-
-        createConnection();
+        System.out.println(checkForMac("ab:cd:ef:01:23:45"));
         
         //insertLVAPstruct(sulec);
         //insertLVAPstruct(hepi);
@@ -297,8 +506,8 @@ public class LVAPserver20 {
         //boolean odpoved = checkIP("45:08:8b:8d:d8:e6");
         //System.out.println(odpoved);
         
-        selectLVAPstruct();
-        
+        //selectLVAPstruct();
+                
         shutdown();
                
     }
